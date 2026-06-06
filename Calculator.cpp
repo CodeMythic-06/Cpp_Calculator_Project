e0c340eda2bc584e15986482c37ec4c88a586cb2
Gideon Oppong <gideonoppong206@gmail.com>
2026-06-06 16:35:46 +0000

    Refactor Calculator.cpp for improved readability

--- Calculator.cpp
+++ Calculator.cpp
@@ -3,42 +3,45 @@
 using namespace std;
 int main() {
 
-  double num1,num2,result;
+  double num1, num2, result;
   char op;
   cout<< "Enter first number "<<endl;
-  cin>>num1;
+  cin>> num1;
   cout<< "Enter operator"<<endl;
-  cin>>op;
+  cin>> op;
   cout<< "Enter second number"<<endl;
-  cin>>num2;
+  cin>> num2;
   
   switch(op) {
     case'+':
     result = num1 + num2;
     cout<< num1 <<op <<num2 <<" = " <<result<<endl;
     break;
+    
     case'-':
     result = num1 - num2;
     cout<< num1 <<op <<num2 <<" = " <<result<<endl;
     break;
+    
     case'*':
     result = num1 * num2;
     cout<< num1 <<op <<num2 <<" = " <<result<<endl;
     break;
+    
     case'/':
     if (num2 == 0){
       cout<< "Maths Error"<<endl;
     }
+      
     else {
       result = num1/num2;
       cout<< num1 <<op <<num2 <<" = " <<result<<endl;
-
-  }
+    }
   break;
 
   default:
   cout<<"Invalid number or operator"<<endl;
   }
 
-
+  return 0;
 }
