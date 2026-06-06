b57a51d9058180b3a500b911b69759b4f2265c7d
Gideon Oppong <gideonoppong206@gmail.com>
2026-06-06 15:53:10 +0000

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,44 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+
+  double num1,num2,result;
+  char op;
+  cout<< "Enter first number "<<endl;
+  cin>>num1;
+  cout<< "Enter operator"<<endl;
+  cin>>op;
+  cout<< "Enter second number"<<endl;
+  cin>>num2;
+  
+  switch(op) {
+    case'+':
+    result = num1 + num2;
+    cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+    break;
+    case'-':
+    result = num1 - num2;
+    cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+    break;
+    case'*':
+    result = num1 * num2;
+    cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+    break;
+    case'/':
+    if (num2 == 0){
+      cout<< "Maths Error"<<endl;
+    }
+    else {
+      result = num1/num2;
+      cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+
+  }
+  break;
+
+  default:
+  cout<<"Invalid number or operator"<<endl;
+  }
+
+
+}
