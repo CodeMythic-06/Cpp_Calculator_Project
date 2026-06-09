3deb7c7e72999e61774b3c2428efe5000036c7b4
Gideon Oppong <gideonoppong206@gmail.com>
2026-06-09 11:57:51 +0000

    Update Calculator.cpp

--- Calculator.cpp
+++ Calculator.cpp
@@ -5,42 +5,43 @@ int main() {
 
   double num1, num2, result;
   char op;
-  cout<< "Enter first number "<<endl;
-  cin>> num1;
-  cout<< "Enter operator"<<endl;
-  cin>> op;
-  cout<< "Enter second number"<<endl;
-  cin>> num2;
+  
+  cout << "Enter first number " << endl;
+  cin >> num1;
+  cout << "Enter operator" << endl;
+  cin >> op;
+  cout <<"Enter second number" << endl;
+  cin >> num2;
   
   switch(op) {
     case'+':
     result = num1 + num2;
-    cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+    cout << num1 <<op << num2 <<" = " << result << endl;
     break;
     
     case'-':
     result = num1 - num2;
-    cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+    cout << num1 << op << num2 <<" = " << result << endl;
     break;
     
     case'*':
     result = num1 * num2;
-    cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+    cout << num1 <<op << num2 <<" = " <<result << endl;
     break;
     
     case'/':
-    if (num2 == 0){
-      cout<< "Division any number by zero is not allowed"<<endl;
+    if (num2 == 0) {
+      cout << "Division any number by zero is not allowed" << endl;
     }
       
     else {
       result = num1/num2;
-      cout<< num1 <<op <<num2 <<" = " <<result<<endl;
+      cout << num1 <<op <<num2 <<" = " << result << endl;
     }
   break;
 
   default:
-  cout<<"Invalid number or operator"<<endl;
+  cout << "Invalid number or operator" << endl;
   }
 
   return 0;
